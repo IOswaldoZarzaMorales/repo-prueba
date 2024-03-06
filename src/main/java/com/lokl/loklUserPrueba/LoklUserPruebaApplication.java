a54fc1cff977f3dae387ae62ea8833b995ba073e
AKMoralesRamos <dental.morak@gmail.com>
2024-03-06 13:44:42 -0600

    segundo commit

---
+++ src/main/java/com/lokl/loklUserPrueba/LoklUserPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.lokl.loklUserPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoklUserPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoklUserPruebaApplication.class, args);
+	}
+
+}
